47599b3268c4e4b3c07e67b46651b98170ef4e91
christos <papazockp@gmail.com>
2018-05-10 15:17:42 +0200

    second commit

--- src/test/java/com/papazog/simpleproject/verysimple/VerySimpleTest.java
+++ src/test/java/com/papazog/simpleproject/verysimple/VerySimpleTest.java
@@ -1,18 +1,25 @@
 package com.papazog.simpleproject.verysimple;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class VerySimpleTest {
 
 
-	@org.junit.Test
+	@Test
 	public void helloWorldTest(){
-		String world = com.papazog.simpleproject.verysimple.VerySimpleClass.helloWorld();
-
+		String world = VerySimpleClass.helloWorld();
 		System.out.println(world);
-
 		assertEquals(world,"Hello World");
+	}
 
+	@Test
+	public void helloWorldTest2() {
+		String world = VerySimpleClass.helloWorld();
+		System.out.println(world);
+		assertNotEquals(world, "Hell World");
 	}
 
 }
